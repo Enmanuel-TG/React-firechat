df7d14e4661d0453d4aaa7ea0a3fa4a4935be597
Adonys Santos <adonysmundotech@gmail.com>
2021-05-04 00:00:28 -0400

    fix: Cambia nombre a los archivos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import { db, googleAuthProvider, firebase, } from './config/firebase';
-import Button from './compouents/Button';
-
-
+import { db, googleAuthProvider, firebase } from "./config/firebase";
+import Button from "./components/Button";
 
 function App() {
   const singInwithGoogle = async () => {
-
     firebase.auth().useDeviceLanguage();
 
     try {
       await firebase.auth().singInwithPopup(googleAuthProvider);
-
-    }
-    catch (e) {
+    } catch (e) {
       console.error(e.message);
     }
   };
 
-  <br />
-
-
   return (
     <div>
-      <Button onClick={singInwithGoogle}>Inicia Secion con Google
-      </Button>
+      <Button onClick={singInwithGoogle}>Inicia Sesion con Google</Button>
     </div>
   );
-};
+}
 
 export default App;
