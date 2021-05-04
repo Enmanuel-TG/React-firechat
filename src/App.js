69199736ce9117527f9c982c971192a4b396b5ed
Enmanuel-TG <enmanueltaveras06@gmail.com>
2021-05-03 23:42:43 -0400

    boton inicio de secion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import { db, googleAuthProvider, firebase, } from './config/firebase';
+import Button from './compouents/Button';
 
 
 
 function App() {
+  const singInwithGoogle = async () => {
+
+    firebase.auth().useDeviceLanguage();
+
+    try {
+      await firebase.auth().singInwithPopup(googleAuthProvider);
+
+    }
+    catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  <br />
+
+
   return (
-    <div></div>
+    <div>
+      <Button onClick={singInwithGoogle}>Inicia Secion con Google
+      </Button>
+    </div>
   );
 };
 
--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -1,14 +1,24 @@
-import firebase from 'Firebase/ app';
+import firebase from "firebase";
 import 'firebase/auth';
 import 'firebase/firestore';
-import { db, googleAuthProvider, firebase } from './config/Firebase';
-// Incializar firebase
 
-firebase.initializeApp({
- /*
-    Esta informacion te la entregar en tu proyecto firebase y es distinta en cada proyecto.
+const firebaseConfig = {
+    apiKey: "AIzaSyAGGAnxt7K7M6veKv1NwSpiWcvNaKtepSg",
+    authDomain: "firechat-573dd.firebaseapp.com",
+    projectId: "firechat-573dd",
+    storageBucket: "firechat-573dd.appspot.com",
+    messagingSenderId: "1001828762594",
+    appId: "1:1001828762594:web:4de46c43251452b99f6fbb",
+    measurementId: "G-VEM8BM94KN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-        M
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-*/
-});
+export {
+    db,
+    googleAuthProvider,
+    firebase
+}
